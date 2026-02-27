eedb49af4b1d293c5063f4c2a3dd481541135ddb
hounsounouetiennettebaby <hounsounouetiennettebaby@gmail.com>
2026-02-27 08:54:01 +0100

    Produit

---
+++ Boucle15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+ int main(){
+     int N,i,j,sum,prd=1;
+     do{
+        printf("Veuillez saisir un entier positif:");
+        scanf("%d",&N);
+     }while(N<=0);
+     for(i=1;i<=N/2;i++){
+            sum=0;
+        for(j=1;j<=i-1;j++){
+            if(i%j==0){
+                sum +=j;
+            }
+        }
+        if(sum==i){
+            prd=prd*i;
+        }
+
+     }
+    return 0;
+ }
